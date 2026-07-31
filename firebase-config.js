06de2ab10df19b0670288ad909c406f660135c25
nomashae <nomashae@gmail.com>
2026-07-31 11:57:17 +0200

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDbkpl1BqqjefM4Y7Jk0jRIKwAuXj2i-z0",
+  authDomain: "nomashae-5fae8.firebaseapp.com",
+  projectId: "nomashae-5fae8",
+  storageBucket: "nomashae-5fae8.firebasestorage.app",
+  messagingSenderId: "992473110933",
+  appId: "1:992473110933:web:059afbb91769f353517e9c",
+  measurementId: "G-FH86PQ41LH"
+};
